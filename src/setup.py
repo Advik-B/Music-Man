570a5c447f22cd2500d69711be72d4a34095c84f
Advik <advik.b@gmail.com>
2022-04-17 11:58:39 +0530

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,35 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+buildOptions = dict(
+    packages = [
+        'os',
+        'yaml',
+        'datetime',
+        'termcolor',
+        'colorama',
+        'qt_material'
+        ],
+    excludes = ['tkinter'],
+    include_files = ['assets', 'themes'],
+    include_msvcr = True,
+    )
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('__main__.py',
+               base=base,
+               targetName='PulsePlayer.exe',
+               icon='assets/icon.ico',
+               )
+]
+
+setup(
+    name='Pulse Player',
+    version = '0.1',
+    description = 'A one-stop app for all your music needs',
+    options = dict(build_exe = buildOptions),
+    executables = executables
+)
