9e6bab1d60603be2f4bb924af40a1008e7144463
Advik <advik.b@gmail.com>
2022-04-22 19:17:54 +0530

    Update widgets.py

--- src/widgets.py
+++ src/widgets.py
@@ -10,16 +10,15 @@ from PyQt5.uic import loadUi
 from qt_material import list_themes, apply_stylesheet
 import sys
 
+
 class SettingsAppearance(QDialog):
     def __init__(self, parent=None, config: dict = None):
         super().__init__(parent)
         self.setWindowTitle("Settings")
         if config is None:
             config = {
-                
-                'theme': 'Dark Cyan',
-                'scale': '0',
-                
+                "theme": "Dark Cyan",
+                "scale": "0",
             }
         self.initUI(config)
         self.show()
@@ -34,29 +33,31 @@ class SettingsAppearance(QDialog):
         self.themeLabel = self.findChild(QLabel, "themeLabel")
         self.scaleLabel = self.findChild(QLabel, "scaleLabel")
         self.scaleSlider = self.findChild(QSlider, "scaleSlider")
-        self.themeBox.addItems(list_themes())
+        self.themeBox.addItems(self.listThemes())
         self.themeBox.currentTextChanged.connect(self.showTheme)
-        self.setTheme(config['theme'])
-    
+        self.setTheme(config["theme"])
+
     def setTheme(self, theme: str):
-        theme = theme.replace(' ', '_')
+        theme = theme.replace(" ", "_")
         theme = theme.lower()
         theme += ".xml"
         self.showTheme(theme)
-    
-    def showTheme(self):
+
+    def showTheme(self, theme: str):
         pass
-    
+
     def listThemes(self):
         ThemeList = []
         for theme in list_themes():
-            ThemeList.append(theme.replace('_', ' ').removesuffix(".xml"))
+            ThemeList.append(theme.replace("_", " ").removesuffix(".xml"))
         return ThemeList
+
 
 def main():
     app = QApplication(sys.argv)
-    UI = SettingsAppearance(config={"theme": "Dark Cyan", "scale": "1"})
+    UI = SettingsAppearance(config={"theme": "Dark Cyan", "scale": "5"})
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
