2053232e2ac50bc0cf82af064d828a1d4e882509
Advik <advik.b@gmail.com>
2022-10-09 13:58:13 +0530

    Update AppGUI.py

---
+++ src/AppGUI.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout
+from PyQt6.uic import loadUi
+
+import sys
+
+
+class MainGUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.preStartUp()
+        self.show()
+
+    def preStartUp(self):
+        loadUi("design.xml", self)
+        # self.layout_ = self.findChild(QHBoxLayout, "mainLayout")
+        # self.setLayout(self.layout_)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    gui = MainGUI()
+    app.exec()
