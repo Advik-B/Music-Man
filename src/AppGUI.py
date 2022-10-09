435ffe831d39f6bbceee7a9d0c3dab8b28cd6376
Advik <advik.b@gmail.com>
2022-10-09 14:14:52 +0530

    Update AppGUI.py

--- src/AppGUI.py
+++ src/AppGUI.py
@@ -1,4 +1,9 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QHBoxLayout,
+    QPushButton,
+)
 from PyQt6.uic import loadUi
 from PyQt6.QtGui import QPalette, QIcon
 import sys
@@ -12,8 +17,13 @@ class MainGUI(QMainWindow):
 
     def preStartUp(self):
         loadUi("design.xml", self)
-        # self.layout_ = self.findChild(QHBoxLayout, "mainLayout")
-        # self.setLayout(self.layout_)
+        self.search_btn = self.findChild(QPushButton, "search_btn")
+        self.home_btn = self.findChild(QPushButton, "home_btn")
+        self.lib_btn = self.findChild(QPushButton, "lib_btn")
+        self.mbar = self.menuBar()
+        # self.home_btn.setIcon(QIcon("svg/home.svg"))
+        # self.search_btn.setIcon(QIcon("svg/search.svg"))
+        # self.lib_btn.setIcon(QIcon("svg/library.svg"))
 
 
 if __name__ == "__main__":
